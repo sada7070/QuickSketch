bd9c560f635afd260acc3b91b6a73bdbaa579c1e
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-05-29 18:17:33 +0530

    updated

--- apps/client/app/canvas/[roomId]/page.tsx
+++ apps/client/app/canvas/[roomId]/page.tsx
@@ -6,12 +6,29 @@ export default function Canvas() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
-        if(canvasRef.current) {
-            initDraw(canvasRef.current);
+        const canvas = canvasRef.current;
+        if(!canvas) return;
+
+        const resize = () => {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
         }
+        resize();
+        // If the window gets resized (user resizes browser), run resize() again.”
+        window.addEventListener("resize", resize);
+
+        initDraw(canvas);
+
+        // Stop listening to resize events when the component is gone — avoid memory leaks.
+        return () => window.removeEventListener("resize", resize);
     }, [canvasRef])
 
     return <div>
-        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+        <canvas ref={canvasRef} className="fixed top-0 left-0 w-screen h-screen block"></canvas>
+
+        <div className="fixed bottom-0 right-2">
+            <button className="bg-amber-50 text-black pr">Rect</button>
+            <button className="bg-amber-50 text-black">circle</button>
+        </div>
     </div>
 }
--- apps/client/components/draw/index.ts
+++ apps/client/components/draw/index.ts
@@ -1,5 +1,21 @@
+type Shape = {
+    type: "rect";
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+} | {
+    type: "circle";
+    centerX: number;
+    centerY: number;
+    radius: number;
+}
+
 export function initDraw(canvas: HTMLCanvasElement) {
     const context = canvas.getContext("2d");
+
+    let existingShapes: Shape[] = [];
+
     if(!context) {
         return;
     }
@@ -20,25 +36,42 @@ export function initDraw(canvas: HTMLCanvasElement) {
 
     canvas.addEventListener("mouseup", (e) => {
         clicked = false;
+        const width = e.clientX - startX;                        // this clintX/clientY takes the value of coordinates where mouse mouves
+        const height = e.clientY - startY;                       // width and hight calculated by taking diff in mouseup and mousedown
+        
+        existingShapes.push({
+            type: "rect",
+            x: startX,
+            y: startY,
+            height,
+            width
+        });
     });
-
-    // canvas.addEventListener("mouseleave", () => {
-    // clicked = false;
-    // });
-
 
     canvas.addEventListener("mousemove", (e) => {
         if(clicked) {
-            const width = e.clientX - startX;                        // this clintX/clientY takes the value of coordinates where mouse mouves
+            const width = e.clientX - startX;
             const height = e.clientY - startY;
 
-            // Clear the canvas before drawing a new rectangle
-            context.clearRect(0, 0, canvas.width, canvas.height);           // when cleared it is intialized to starting positions
-            context.fillRect(0, 0, canvas.width, canvas.height);
+            clearCanvas(existingShapes, canvas, context);
 
             // drawing rect
             context.strokeStyle  = "rgba(255, 255, 255)";
             context.strokeRect(startX, startY, width, height);
         }
     });
+}
+
+function clearCanvas(existingShapes: Shape[], canvas: HTMLCanvasElement ,context: CanvasRenderingContext2D) {
+    // Clear the canvas before drawing a new rectangle
+    context.clearRect(0, 0, canvas.width, canvas.height);           // when cleared it is intialized to starting positions
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    existingShapes.map((shape) => {
+        if(shape.type == "rect") {
+            // drawing rect
+            context.strokeStyle  = "rgba(255, 255, 255)";
+            context.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    }); 
 }
